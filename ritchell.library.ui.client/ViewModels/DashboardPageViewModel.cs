dd654c9663c9537565d7ab1c85b1fe3ad06ecb77
Aqudei <fdeu.aalmine@gmail.com>
2016-01-10 07:19:54 +0800

    added some client code

---
+++ ritchell.library.ui.client/ViewModels/DashboardPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace ritchell.library.ui.client.ViewModels
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class DashboardPageViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the DashboardPageViewModel class.
+        /// </summary>
+        public DashboardPageViewModel()
+        {
+        }
+    }
+}
