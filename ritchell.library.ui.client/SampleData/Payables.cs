985778e4e1c6638443c4bfaf5624f28e25f0f513
Aqudei <fdeu.aalmine@gmail.com>
2016-01-30 05:51:19 +0800

    minor fixes

---
+++ ritchell.library.ui.client/SampleData/Payables.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ritchell.library.ui.client.SampleData
+{
+    public class Payables : List<model.Payable>
+    {}
+}
